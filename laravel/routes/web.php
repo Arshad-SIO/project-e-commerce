61d2a436e53224bcf2ef099d0e523938bfb537e7
Arshad-SIO <arshad.umair.sio1@gmail.com>
2024-11-19 15:20:40 +0100

    Creer des routes et des controleurs

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientController ;
+use App\Http\Controllers\ProduitController ;
+use App\Http\Controllers\PanierController ;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +17,21 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return "Projet E-COMMERCE";
 });
+
+Route::get('/clients/connexion', [ ClientController::class, 'connexion' ] ) ;
+
+Route::get('/clients/deconnexion', [ ClientController::class, 'deconnexion' ] ) ;
+
+Route::get('/clients/panier', [ ClientController::class, 'panier' ] ) ;
+
+Route::get('/produits', [ ProduitController::class, 'produit' ] ) ;
+
+Route::get('/produits/description', [ ProduitController::class, 'description' ] ) ;
+
+Route::get('/produits/panier', [ ProduitController::class, 'panier' ] ) ;
+
+Route::get('/panier/commande', [ PanierController::class, 'commande' ] ) ;
+
+Route::get('/panier/commande/connexion/paiement', [ PanierController::class, 'paiement' ] ) ;
